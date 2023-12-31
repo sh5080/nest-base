77bd3a3c9ca91d302bbcd7252f85b15caa5bcea2
sh5080 <prpn97@gmail.com>
2024-01-01 01:31:41 +0900

    fix: 전역으로 사용 x

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { authConfig } from '../config/env.config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
@@ -8,17 +7,7 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { AuthRepository } from './auth.repository';
 
 @Module({
-  imports: [
-    JwtModule.registerAsync({
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('ACCESS_JWT_SECRET'),
-        signOptions: { expiresIn: '1d' },
-      }),
-      inject: [ConfigService],
-    }),
-    ConfigModule.forFeature(authConfig),
-    PrismaModule,
-  ],
+  imports: [ConfigModule.forFeature(authConfig), PrismaModule],
   providers: [AuthService, AuthRepository],
   exports: [AuthService],
   controllers: [AuthController],
